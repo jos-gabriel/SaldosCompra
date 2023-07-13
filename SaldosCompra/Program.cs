348d6797cb374c76b737ea5c106c047836ac77e4
jos-gabriel <jos.gabrie@gmail.com>
2023-07-12 21:54:34 -0600

    Agregar archivos de proyecto.

---
+++ SaldosCompra/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double montoInicial = 13000.0;
+        double montoFinal = 0.0;
+
+        UInt16 Tcompras = Convert.ToUInt16(Console.ReadLine());
+        double[] ArrayCompras;
+
+        ArrayCompras = new double[Tcompras];
+
+        for (UInt16 f = 0; f < Tcompras; f++)
+        {
+            Console.WriteLine("Ingrese valor de la compra:");
+
+            //ArrayCompras[f] =  Convert.ToDouble(Console.ReadLine());
+
+            montoInicial -= Convert.ToDouble(Console.ReadLine());
+
+
+        }
+
+
+
+        Console.WriteLine(montoInicial);
+
+    }
+}
